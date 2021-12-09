53fc1e22b17b32f1bd12dd8798fc9c60d20f82cd
squidtempura <55754978+squidtempura@users.noreply.github.com>
2021-12-09 03:25:17 +0000

    set different materials to spheres

--- MusicVisualisation/Assets/Scripts/GenerateSpheres.cs
+++ MusicVisualisation/Assets/Scripts/GenerateSpheres.cs
@@ -36,17 +36,49 @@ public class GenerateSpheres : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Renderer render = spherePrefab.GetComponent<Renderer>();
         for(int i = 0; i < 512; i++)
         {
-            for (int j = 0; j<7;j++)
-            {
-                if(spheres != null)
+            if (spheres != null) {
+				GameObject sphere = spheres [i];
+				Renderer rend = sphere.GetComponent<Renderer>();
+				rend.enabled = true;
+                int j;
+                if(i<=64)
                 {
-                    render.sharedMaterial = materials[j];
+                    j = 0;
+                }else if(i <= 128)
+                {
+                    j = 1;
                 }
+                else if(i <= 192)
+                {
+                    j = 2;
+                }
+                else if(i <= 256)
+                {
+                    j = 3;
+                }
+                else if(i <= 320)
+                {
+                    j = 4;
+                }
+                else if(i <= 384)
+                {
+                    j = 5;
+                }
+                else if(i <= 448)
+                {
+                    j = 6;
+                }
+                else
+                {
+                    j = 7;
+                }
+                
+                rend.sharedMaterial = materials[j]; 
+                Transform s = sphere.transform;
             }
-        }
         
+        }
     }
 }
